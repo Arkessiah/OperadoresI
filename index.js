52488af1166adadccc8e9284704b06d4f6404439
Arkessiah <amarotoenriquez@gmail.com>
2021-11-25 16:00:44 +0000

    finish

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Declara dos variables numeroA y numeroB con los valores 5 y 7 respectivamente.
-
+var numeroA = 5;
+var numeroB = 7;
 
 // Declara una variable sumaTotal que sea igual a la suma de numeroA y numeroB
-
+var sumaTotal = numeroA + numeroB;
 
 // Declara una variable restaTotal que sea igual a la resta de numeroB menos numeroA
-
+var restaTotal = numeroB - numeroA;
 
 // Declara una variable multiTotal que sea igual a la multiplicación de sumaTotal por restaTotal
-
+var multiTotal = sumaTotal * restaTotal;
 
 // Declara una variable divisionTotal que sea igual a la división de multiTotal entre restaTotal
 
+var divisionTotal = multiTotal / restaTotal;
